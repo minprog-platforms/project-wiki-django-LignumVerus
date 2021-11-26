f1f53b2ae317260f9e5efe4a6a056dc0bed91558
LignumVerus <peranovicfinn@gmail.com>
2021-11-26 16:43:46 +0100

    added search function, and prototyped adding new pages

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("newpage/", views.newpage, name="newpage"),
+    path("wiki/<str:article>", views.entrypage, name="entrypage"),
+    path("error", views.errorpage, name="errorpage"),
+    path("search", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
+from logging import error
 from django.shortcuts import render
+from django import forms
+from markdown2 import Markdown
 
 from . import util
 
@@ -8,3 +11,57 @@ def index(request):
         "entries": util.list_entries()
     })
 
+class NewPageForm(forms.Form):
+    title = forms.CharField(label="title", strip=True)
+    text = forms.CharField(label="text", strip=True)
+
+def newpage(request):
+    #title = request.POST.get("new_title")
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            text = form.cleaned_data["text"]
+            util.save_entry(title, text)
+
+    return render(request, "encyclopedia/newpage.html", {
+        "form": NewPageForm()
+    })
+
+def entrypage(request, article):
+    try:
+        raw_text = util.get_entry(article)
+        markdowner = Markdown()
+
+        text = markdowner.convert(raw_text)
+
+        return render(request, "encyclopedia/entrypage.html", {
+        "text": text
+        })
+    except:
+        return errorpage(request)
+
+def errorpage(request):
+    return render(request, "encyclopedia/errorpage.html")
+
+def search(request):
+    searched = request.POST.get("q")
+    searched_lower = searched.lower()
+
+    articles = util.list_entries()
+    articles_lower = list(map(lambda article: article.lower(), articles))
+
+    i = 0
+    for article_lower in articles_lower:
+        if str(searched_lower) == article_lower:
+            return entrypage(request, articles[i])
+
+        i += 1
+
+    # else
+    results = [article for article in articles if searched_lower in article.lower()]
+
+    return render(request, "encyclopedia/search.html", {
+        "results": results
+    })
